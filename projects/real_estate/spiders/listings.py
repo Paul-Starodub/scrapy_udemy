c105e98128af9b4f1e1bef52a86c49bebc2477d9
Pavlo Starobub <amorallex@gmail.com>
2026-06-03 11:13:19 +0300

    Refactor listings spider

--- projects/real_estate/spiders/listings.py
+++ projects/real_estate/spiders/listings.py
@@ -1,10 +1,23 @@
 import scrapy
 
+from real_estate.items import RealEstateItem
+
 
 class ListingsSpider(scrapy.Spider):
     name = "listings"
     allowed_domains = ["arizonarealestate.com"]
-    start_urls = ["https://arizonarealestate.com"]
+    start_urls = [
+        "https://www.arizonarealestate.com/maricopa/",
+        "https://www.arizonarealestate.com/goodyear/",
+        "https://www.arizonarealestate.com/tempe/",
+    ]
 
     def parse(self, response):
-        pass
+        gallery = response.xpath('//div[@class="si-listings-column"]')
+        for listing in gallery:
+            item = RealEstateItem()
+            item["name"] = listing.xpath('.//div[@class="si-listing__title-main"]/text()').get()
+            item["description"] = listing.xpath('.//div[@class="si-listing__title-description"]/text()').get()
+            item["price"] = listing.xpath('.//div[@class="si-listing__photo-price"]/span/text()').get()
+            item["agency"] = listing.xpath('.//div[@class="si-listing__footer"]/div/text()').get()
+            yield item
