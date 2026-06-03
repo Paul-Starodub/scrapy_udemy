dc185dc394af023c0ef443d00da707f98b2121eb
Pavlo Starobub <amorallex@gmail.com>
2026-06-03 14:38:38 +0300

    add input processor for description

--- projects/real_estate/items.py
+++ projects/real_estate/items.py
@@ -4,11 +4,16 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose
+
+
+def description_in(description):
+    return description.strip()
 
 
 class RealEstateItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
-    description = scrapy.Field()
+    description = scrapy.Field(input_processor=MapCompose(description_in))
     price = scrapy.Field()
     agency = scrapy.Field()
